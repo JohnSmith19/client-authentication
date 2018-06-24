5c5f643aa0c7c47b30f38df0edea190ec884a97b
John Smith <unknown.prop@gmail.com>
2018-06-24 10:50:37 +0900

    Creating the Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
-  return <div>Hi, App</div>;
+  return (
+    <div>
+      <Header />
+      Hi, App
+    </div>
+  );
 };
 
 export default App;
